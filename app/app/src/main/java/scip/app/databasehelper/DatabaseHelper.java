000b8cb51b7c85615c819ec9b1a4c07f33c20453
Allie Deford <allied935@gmail.com>
2015-06-22 14:29:06 -0700

    Create couple model

--- app/app/src/main/java/scip/app/databasehelper/DatabaseHelper.java
+++ app/app/src/main/java/scip/app/databasehelper/DatabaseHelper.java
@@ -2,6 +2,8 @@ package scip.app.databasehelper;
 
 /**
  * Created by Allie on 6/22/2015.
+ *
+ * This class is designed to make performing database operations easier. It will contain methods for all direct database access actions.
  */
 public class DatabaseHelper {
 }
--- app/app/src/main/java/scip/app/models/Couple.java
+++ app/app/src/main/java/scip/app/models/Couple.java
@@ -2,6 +2,28 @@ package scip.app.models;
 
 /**
  * Created by Allie on 6/22/2015.
+ *
+ * This is the model for the Couple data structure.
  */
 public class Couple {
+    int id;
+    // eventually will contain information about peak fertility
+
+    // Constructors
+    public Couple() {
+    }
+
+    public Couple(int id) {
+        this.id = id;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
 }
