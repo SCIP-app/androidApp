5bfa2b73e8ef55d1bd9f7814764fd99cf61b1c6f
Allie Deford <allied935@gmail.com>
2015-06-22 14:18:24 -0700

    Add new packages and files for handling database

---
+++ app/app/src/main/java/scip/app/databasehelper/DatabaseHelper.java
@@ -0,0 +1,7 @@
+package scip.app.databasehelper;
+
+/**
+ * Created by Allie on 6/22/2015.
+ */
+public class DatabaseHelper {
+}
---
+++ app/app/src/main/java/scip/app/models/Couple.java
@@ -0,0 +1,7 @@
+package scip.app.models;
+
+/**
+ * Created by Allie on 6/22/2015.
+ */
+public class Couple {
+}
